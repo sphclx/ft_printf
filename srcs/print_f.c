18b4e87515d46c465773b665fb1da53a6b8f8222
Anivia Benton <abenton@mi-p5.21-school.ru>
2019-11-14 14:26:22 +0300

    u, o, x

--- srcs/print_f.c
+++ srcs/print_f.c
@@ -1,24 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_s.c                                          :+:      :+:    :+:   */
+/*   print_f.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenton <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/14 12:26:39 by abenton           #+#    #+#             */
-/*   Updated: 2019/11/14 13:07:53 by abenton          ###   ########.fr       */
+/*   Created: 2019/11/14 14:16:26 by abenton           #+#    #+#             */
+/*   Updated: 2019/11/14 14:25:59 by abenton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-t_printf	print_s(t_printf	*tab)
+t_printf	print_f(t_printf *tab)
 {
-	char	*s;
 
-	if (ft_strcmp(tab->flag, "l") == 0)
-		s = (wchar_t *)va_arg(tab->args, wchar_t *);
-	else
-		s = (char *)va_arg(tb->args, char *);
 	return (tab);
 }
---
+++ srcs/print_u.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_u.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/14 13:08:08 by abenton           #+#    #+#             */
+/*   Updated: 2019/11/14 14:03:27 by abenton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+t_printf	print_u(t_printf *tab)
+{
+	unsigned int	num;
+
+	if (ft_strcmp(tab->argument_flag, "hh") == 0)
+		num = (unsigned char)(va_arg(tab->args, unsigned int));
+	else if (ft_strcmp(tab->argument_flag, "h") == 0)
+		num = (unsigned short)(va_arg(tab->args, unsigned int));
+	else if (ft_strcmp(tab->argument_flag, "ll") == 0)
+		num = (unsigned long long)(va_arg(tab->args, unsigned long long int));
+	else if (ft_strcmp(tab->argument_flag, "l") == 0)
+		num = (unsigned long)(va_arg(tab->args, unsigned long int));
+	else if (ft_strcmp(tab->argument_flag, "j") == 0)
+		num = (uintmax_t)(va_arg(tab->args, uintmax_t));
+	else if (ft_strcmp(tab->argument_flag, "z") == 0)
+		num = (size_t)(va_arg(tab->args, size_t));
+	else
+		num = (unsigned int)(va_arg(tab->args, unsigned int));
+		return (num);
+}
