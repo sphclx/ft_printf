fd0c0ec1e45d1162879dcb335f70f9185805ed57
Anivia Benton <abenton@mi-p5.21-school.ru>
2019-11-15 15:26:07 +0300

    print_s

--- srcs/print_s.c
+++ srcs/print_s.c
@@ -3,35 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   print_s.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abenton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: mburl <mburl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 12:26:39 by abenton           #+#    #+#             */
-/*   Updated: 2019/11/15 14:46:54 by abenton          ###   ########.fr       */
+/*   Updated: 2019/11/15 15:25:56 by abenton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		print_s(t_flags	*flags)
+int		print_s(t_flags	*flags, va_list args)
 {
 	char	*s;
-	int		i;
 	int		len;
 
-	if (flags->len == LEN_TYPE_L)
-		s = (wchar_t *)s;
-	else
-		s = (char *)s;
+	s = va_arg(args, char *);
+//	if (flags->len == LEN_TYPE_L)
+//		s = (wchar_t *)s;
+//	else
+//		s = (char *)s;
 	if (flags->precision > 0 && s)
-		s =ft_strndup(flags->presicion, s);
+		s = strndup(flags->precision, s);	//FT_STRDUP
 	else if (flags->precision == 0 && s)
 		s = ft_strdup(s);
 	else if (flags->precision > 0 && !s)
-		s = ft_strndup("(null)", flags->precision);
+		s = strndup("(null)", flags->precision); // FT_STRDUP
 	else if (flags->precision == 0 && !s)
 		s = ft_strdup("(null)");
 	len = ft_strlen(s);
 	flags->len += len;
-	ft_putstr(s);
-	return (flags);
+	s += ft_pad(flags, len);
+	ft_write(s, (int)ft_strlen(s), flags);
+	free(s);
+	return (0);
 }
